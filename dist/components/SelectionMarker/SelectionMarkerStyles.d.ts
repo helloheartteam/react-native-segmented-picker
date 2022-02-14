610f4b5b148a1288ae92bf6c1ba85e77e98cc043
HadasKedem <hadask96@gmail.com>
2022-02-14 13:26:04 +0200

    new dist folder with the style changes

---
+++ dist/components/SelectionMarker/SelectionMarkerStyles.d.ts
@@ -0,0 +1,21 @@
+declare const _default: {
+    selectionMarkerContainer: {
+        width: string;
+        height: string;
+        position: "absolute";
+        top: number;
+        left: number;
+        flex: number;
+        alignItems: "center";
+        justifyContent: "center";
+    };
+    selectionMarkerBorder: {
+        width: string;
+        height: number;
+    };
+    selectionMarker: {
+        width: string;
+        height: number;
+    };
+};
+export default _default;
---
+++ dist/components/Toolbar/ToolbarStyles.d.ts
@@ -0,0 +1,24 @@
+declare const _default: {
+    toolbarContainer: {
+        width: string;
+        height: number;
+        borderBottomWidth: number;
+        flexDirection: "row";
+        justifyContent: "flex-end";
+        alignItems: "center";
+        alignSelf: "flex-start";
+    };
+    toolbarConfirmContainer: {
+        height: string;
+        paddingLeft: number;
+        justifyContent: "center";
+    };
+    toolbarConfirmText: {
+        fontSize: number;
+        paddingTop: number;
+        paddingRight: number;
+        paddingBottom: number;
+        paddingLeft: number;
+    };
+};
+export default _default;
